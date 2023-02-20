06af653932261e8631a5c4f13ac6b8ef5fcba5bb
Rahul Arora <108695777+Rahul12Arora@users.noreply.github.com>
2023-02-21 02:27:05 +0530

    done

--- color_flipper/color_flipper.js
+++ color_flipper/color_flipper.js
@@ -7,14 +7,24 @@ buttonElement.textContent = "Click Me";
 //add button to body
 bodyElement.appendChild(buttonElement);
 
-// const colorArray = ["red", "green", "blue", "teal", "pink", "black", "purple", "yellow", "white", "orange"];
+
+//create a div element to show color
+const infoElement = document.createElement("p");
+infoElement.className = "colorinfo"
+infoElement.textContent = "Background Color: #000000";
+//add button to body
+bodyElement.appendChild(infoElement);
+
+
 
 function setBackgroundColor(){
     // console.log("insisde set background color function");
     let num = Math.floor(Math.random()*10);
     console.log(num);
     // bodyElement.style.backgroundColor = colorArray[num];
-    bodyElement.style.backgroundColor = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    const colorval = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    bodyElement.style.backgroundColor = colorval;
+    infoElement.innerHTML= "Background Color: "+colorval;
 }
 
 buttonElement.addEventListener("click", setBackgroundColor);
