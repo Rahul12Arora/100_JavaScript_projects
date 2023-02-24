392c66e72f7df2efd4d382cbb6c5334560867567
Rahul Arora <108695777+Rahul12Arora@users.noreply.github.com>
2023-02-24 17:40:39 +0530

    update

---
+++ 7_Snappy_News_Application/app.js
@@ -0,0 +1,27 @@
+const inputele = document.querySelector(".inputfield");
+const buttonele = document.querySelector(".button");
+const subframeele = document.querySelector(".subframe");
+
+
+buttonele.addEventListener("click",getimage);
+function getimage(){
+    console.log("entered in button");
+    fetch("https://inshorts.deta.dev/news?category=technology").then((resolvedapidata)=>{
+    return resolvedapidata.json();
+}).then((jsonfile)=>{
+    console.log("recieved json file");
+    return jsonfile.data[0].imageUrl;
+}).then((imageurl)=>{
+    console.log("recieved image url as " + imageurl);
+    const imageboxele = document.createElement('div');
+    imageboxele.className="imagebox";
+    subframeele.appendChild(imageboxele);
+    const imgele = document.createElement('img');
+    imgele.className="image";
+    imgele.src = imageurl;
+    imageboxele.appendChild(imgele);
+    console.log("the end");
+}).catch((errormessage)=>{
+    console.log(errormessage);
+})
+}
