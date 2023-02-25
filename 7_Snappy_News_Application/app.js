8a6195897d74a8fff9dfe9b40f96d60ddbf8b7c6
Rahul Arora <108695777+Rahul12Arora@users.noreply.github.com>
2023-02-25 11:23:05 +0530

    update

--- 7_Snappy_News_Application/app.js
+++ 7_Snappy_News_Application/app.js
@@ -1,27 +1,32 @@
 const inputele = document.querySelector(".inputfield");
-const buttonele = document.querySelector(".button");
+const buttonele = document.querySelector(".button-wrapper");
 const subframeele = document.querySelector(".subframe");
 
 
-buttonele.addEventListener("click",getimage);
-function getimage(){
-    console.log("entered in button");
-    fetch("https://inshorts.deta.dev/news?category=technology").then((resolvedapidata)=>{
-    return resolvedapidata.json();
-}).then((jsonfile)=>{
-    console.log("recieved json file");
-    return jsonfile.data[0].imageUrl;
-}).then((imageurl)=>{
-    console.log("recieved image url as " + imageurl);
-    const imageboxele = document.createElement('div');
-    imageboxele.className="imagebox";
-    subframeele.appendChild(imageboxele);
-    const imgele = document.createElement('img');
-    imgele.className="image";
-    imgele.src = imageurl;
-    imageboxele.appendChild(imgele);
-    console.log("the end");
-}).catch((errormessage)=>{
-    console.log(errormessage);
+// buttonele.addEventListener("click",getimage);
+// function getimage(){
+//     console.log("entered in button");
+//     fetch("https://inshorts.deta.dev/news?category=technology").then((resolvedapidata)=>{
+//     return resolvedapidata.json();
+// }).then((jsonfile)=>{
+//     console.log("recieved json file");
+//     return jsonfile.data[0].imageUrl;
+// }).then((imageurl)=>{
+//     console.log("recieved image url as " + imageurl);
+//     const imageboxele = document.createElement('div');
+//     imageboxele.className="imagebox";
+//     subframeele.appendChild(imageboxele);
+//     const imgele = document.createElement('img');
+//     imgele.className="image";
+//     imgele.src = imageurl;
+//     imageboxele.appendChild(imgele);
+//     console.log("the end");
+// }).catch((errormessage)=>{
+//     console.log(errormessage);
+// })
+// }
+
+buttonele.addEventListener("click",()=>{
+    console.log("window should be redirected");
+    window.location.href = "http://www.w3schools.com";
 })
-}
